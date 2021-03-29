6bcba25766bafd718ee2d4dc0144d7b407e7e32b
jamieliao-byte <jamieliao@hotmail.com>
2021-03-29 20:48:57 +0000

    added loop

---
+++ advlist/animal-list.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+
+# create a list called list1
+animalList = ["Fox","Fly","Ant","Bee","Cod","Cat","Dog","Yak","Cow","Hen","Koi","Hog","Jay","Kit"]
+# display list1
+
+for x in animalList:
+    print(x)
+
