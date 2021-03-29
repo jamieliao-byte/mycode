35cab795bab753ef5d43c433898521253075c9d7
jamieliao-byte <jamieliao@hotmail.com>
2021-03-29 19:18:21 +0000

    class lab

---
+++ lab_input/iptaker01.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+'''lab exercise for user input '''
+
+def main():
+
+    user_input = input("Please enter an IPv4 IP address:")
+
+    ## the line below creates a single string that is passed to print()
+    # print("You told me the IPv4 address is:" + user_input)
+
+    ## print() can be given a series of objects separated by a comma
+    print("You told me the IPv4 address is:", user_input)
+
+main()
