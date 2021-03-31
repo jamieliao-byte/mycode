2bbafdc55195fb92e86a03860566ac2e739d0fd3
jamieliao-byte <jamieliao@hotmail.com>
2021-03-31 19:27:50 +0000

    day3 afternoon

---
+++ lab_pylint/smallscript_better.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+
+''' documentation '''
+
+def main():
+    """Our main program"""
+    mynote = 'print this string' # define mynote
+    print(mynote)                # display our string
+
+main()
